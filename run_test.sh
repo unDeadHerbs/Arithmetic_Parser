3e9d75a8a6be385ccff9a2b15f5cc1383db73388
Murray Fordyce <unDeadHerbs@gmail.com>
2019-04-17 12:17:53 -0500

    Improved test runner

--- run_test.sh
+++ run_test.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-ls tests | sed 's/[.]\(in\|out\)//' | uniq |
+set -e
+
+# Needs atleast one of `.in`, `.file`, or `.out`.
+
+if [ $# -ne 0 ]; then
+    echo "This script takes no arguments."
+    echo "All tests are infered from the tests/ directory"
+    echo "A test will be run if it has any of the following"
+    echo " 1) An input file \"bin_name.input\""
+    echo " 2) A parameter file \"bin_name.file\""
+    echo " 3) An output file \"bin_name.output\""
+    exit 0
+fi
+
+ls tests | sed 's/[.]\(input\|file\|output\)//' | uniq |
     while read r; do
 	echo "Starting test $r"
 	# existance of file
-	[ -e "$(echo $r | sed 's/[.].*/.bin/')"  ] || { echo "Unable to find $(echo $r | sed 's/[.].*/.bin/')" ; exit 1 ; }
-	# existance of input
-	[ -e "$(echo $r | sed 's/.*/tests\/&.in/')"  ] || { echo "Unable to find $(echo $r | sed 's/.*/&.in/')" ; exit 1 ; }
+	[ -e "$(echo $r | sed 's/[.].*//').bin"  ] || { echo "Unable to find $(echo $r | sed 's/[.].*//').bin" ; exit 1 ; }
 	# existance of output
-	if [ -e "$(echo $r | sed 's/.*/tests\/&.out/')"  ] ; then
-	    timeout 2 $(echo $r | sed 's/[.].*/.bin/'|sed 's/.*/.\/&/') $(echo $r | sed 's/.*/tests\/&.in/') 2>&1 |
-		diff -baC 1 - $(echo $r | sed 's/.*/tests\/&.out/') ||
-		{ echo "\n--- From Input ---\n" ; cat $(echo $r | sed 's/.*/tests\/&.in/') ; exit 1 ; }
+	if [ -e "tests/$r.input" ]; then
+	    cat "test/$r.input"
+	fi |
+	timeout 2 "./$(echo $r|sed 's/[.].*//').bin" $([ -e "tests/$r.file" ] && \
+				 echo "tests/$r.file") 2>&1 |
+	if [ -e "tests/$r.output" ]; then
+	    if diff -baC 1 - "tests/$r.output"; then
+		cat
+	    else
+		if [ -e "tests/$r.input" ]; then
+		   echo "--- Pipe Input ---"
+		   cat "tests/$r.input"
+		fi
+		if [ -r "tests/$r.file" ]; then
+		   echo "--- File Input ---"
+		   cat "tests/$r.file"
+		fi
+		exit 1
+	    fi
 	else
-	    echo
-	    timeout 2 $(echo $r | sed 's/[.].*/.bin/'|sed 's/.*/.\/&/') $(echo $r | sed 's/.*/tests\/&.in/') 2>&1
-	    echo
+	    # if command failed
+	    cat
+	    # exit 1
+	    # fi
 	fi
 	echo "Test $r compleate."
     done
-# list test files
-# for each test/x.#.in find x.bin
-#  print #
-#  diff (x.bin test/x.#.in) with test/x.#.out
