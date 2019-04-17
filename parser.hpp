9c71c8824119f3cc68281e7e61bf4c43596b3cb8
Murray Fordyce <Murray.Fordyce@gmail.com>
2019-04-17 02:12:46 +0000

    new formating

--- parser.hpp
+++ parser.hpp
@@ -1,11 +1,11 @@
 #ifndef __PARSER_HPP__
 #define __PARSER_HPP__
 
+#include <string>
 #include <variant>
 #include <vector>
-#include <string>
-#include "code_tree.hpp"
 #include "Tokenizer/tokenizer.hpp"
+#include "code_tree.hpp"
 
 Code_Tree parse(std::vector<Token>&);
 
